b5b6d87501ceb08fe22c5fa3f4c1639095844805
wolfpoland <wolfpoland1@gmail.com>
2016-07-08 12:20:16 +0200

    PostepPrac
    postepo

--- projektUF2/Models/Service/Komunikacja.cs
+++ projektUF2/Models/Service/Komunikacja.cs
@@ -7,14 +7,33 @@ namespace projektUF2.Models.Service
 {
     public class Komunikacja
     {
+        private const string CacheKey = "KontaktStore";
         public List<Kontakt> kom = new List<Kontakt>();
         public Komunikacja()
         {
-            kom.Add(new Kontakt(1, "Patryk", "Krasuski"));
+            var ctx = HttpContext.Current;
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    kom.Add(new Kontakt(1, "Patryk", "Krasuski"));
+                    kom.Add(new Kontakt(2, "Alicja", "ZkrainyCzarwow"));
+                    ctx.Cache[CacheKey] = kom.ToArray();
+
+                }
+            }
+           
         }
         public Kontakt[] getAll()
         {
-            return kom.ToArray();
+            var ctx = HttpContext.Current;
+            if(ctx != null)
+            {
+                return (Kontakt[])ctx.Cache[CacheKey];
+            }
+            List<Kontakt> kontakty = new List<Kontakt>();
+            kontakty.Add(new Kontakt(1, "PLACEHOLDER", "PlaceHoleder"));
+            return kontakty.ToArray();
         }
     }
 }
