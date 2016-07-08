07aa0ef9df82fc5df69bcb251294f8d6a17c6b41
wolfpoland <wolfpoland1@gmail.com>
2016-07-08 10:50:37 +0200

    startowy
    commit startowy

---
+++ projektUF2/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace projektUF2.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
