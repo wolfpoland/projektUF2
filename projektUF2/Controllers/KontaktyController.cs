a00ea60517db87ce61ff21245de2f1b5e28bb65f
wolfpoland <wolfpoland1@gmail.com>
2016-07-08 11:40:19 +0200

    ZaczynamTestowanie
    Testowanie sie rozpoczyna

---
+++ projektUF2/Controllers/KontaktyController.cs
@@ -0,0 +1,23 @@
+﻿using projektUF2.Models;
+using projektUF2.Models.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace projektUF2.Controllers
+{
+    public class KontaktyController : ApiController
+    {
+        public Komunikacja komunikat;
+        public KontaktyController()
+        {
+            this.komunikat = new Komunikacja();
+        }
+        public Kontakt[] Get() {
+            return komunikat.getAll();
+        }
+    }
+}
---
+++ projektUF2/Models/Kontakt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projektUF2.Models
+{
+    public class Kontakt
+    {
+        public Kontakt(int id,string imie, string nazwisko)
+        {
+            this.id = id;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+        public int id { get; set; }
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+    }
+}
