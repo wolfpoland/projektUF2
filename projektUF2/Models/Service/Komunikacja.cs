e1e66fb28b33dc1399ef6d8870fe2e3db4e92202
wolfpoland <wolfpoland1@gmail.com>
2016-07-08 11:41:39 +0200

    Dodatkow do testowanie
    dodatkowe testowanie

---
+++ projektUF2/Models/Service/Komunikacja.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projektUF2.Models.Service
+{
+    public class Komunikacja
+    {
+        public List<Kontakt> kom = new List<Kontakt>();
+        public Komunikacja()
+        {
+            kom.Add(new Kontakt(1, "Patryk", "Krasuski"));
+        }
+        public Kontakt[] getAll()
+        {
+            return kom.ToArray();
+        }
+    }
+}
