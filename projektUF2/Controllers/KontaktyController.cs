108bf9281d3ff1649ec501b14051ce323a7209cd
Patryk <wolfpoland1@gmail.com>
2016-07-08 13:45:11 +0200

    Kolejna werska
    Dalej bledow masa

--- projektUF2/Controllers/KontaktyController.cs
+++ projektUF2/Controllers/KontaktyController.cs
@@ -16,6 +16,7 @@ namespace projektUF2.Controllers
         {
             this.komunikat = new Komunikacja();
         }
+        [HttpGet]
         public Kontakt[] Get() {
             return komunikat.getAll();
         }
--- projektUF2/Models/Service/Komunikacja.cs
+++ projektUF2/Models/Service/Komunikacja.cs
@@ -35,5 +35,26 @@ namespace projektUF2.Models.Service
             kontakty.Add(new Kontakt(1, "PLACEHOLDER", "PlaceHoleder"));
             return kontakty.ToArray();
         }
+        public bool saveKontakt(Kontakt kontakt)
+        {
+            var ctx = HttpContext.Current;
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Kontakt[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(kontakt);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+                    return true;
+                }catch(Exception e)
+                {
+                    Console.WriteLine("Cos poszlo nie tak kowboju");
+                    return false;
+                }
+                
+
+            }
+            return false;
+        }
     }
 }
